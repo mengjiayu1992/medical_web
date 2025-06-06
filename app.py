a7d3f26899176979e74707acf34d849751035e2c
mengjiayu1992 <jasonmengjiayu@gmail.com>
2025-06-06 15:14:52 -0400

    update app.py for Render deployment

--- app.py
+++ app.py
@@ -90,5 +90,8 @@ def edit():
 
     return render_template("edit.html", cpt_map=CPT_MAP)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
